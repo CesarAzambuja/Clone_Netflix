955513e1ed9530892869b30739f13eab6b8336b2
César Oliveira <cesar.oliveira@takelog.com.br>
2020-08-28 14:57:59 -0300

    Populando o carrusel de filmes atravez do javascript

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -1,3 +1,111 @@
+const filmes = document.querySelector('#filme-cartaz')
+const urlsCartaz = [
+    {
+        link:"https://image.tmdb.org/t/p/w220_and_h330_face/oaLXsduGoDOOftWziIOvyZzrR4T.jpg",
+        nome: "Arrow"
+    }, 
+
+    {
+        link:"https://image.tmdb.org/t/p/w220_and_h330_face/xLxgVxFWvb9hhUyCDDXxRPPnFck.jpg",
+        nome:"joker"
+    },
+
+    {
+        link:"https://image.tmdb.org/t/p/w220_and_h330_face/geyu6rplpbp7OUeOfB2uRVf1LpG.jpg",
+        nome:"Liga da Justiça"
+    },
+
+    {
+        link:"https://image.tmdb.org/t/p/w220_and_h330_face/f496cm9enuEsZkSPzCwnTESEK5s.jpg",
+        nome:"Friends"
+    },
+
+    {
+        link:"https://image.tmdb.org/t/p/w220_and_h330_face/811j0Jf2D0mK1U6RxXJoZgOB29n.jpg",
+        nome:"Harry Potter"
+    },
+
+    {
+        link:"https://image.tmdb.org/t/p/w220_and_h330_face/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg",
+        nome: "Flash"
+    },
+
+    {
+        link:"https://image.tmdb.org/t/p/w220_and_h330_face/38teDX74nsxkv2ysWvNT5EPXQ9E.jpg",
+        nome:"Power"
+    },
+
+    {
+        link:"https://image.tmdb.org/t/p/w220_and_h330_face/64gq5GavH3wAIxbFeyE5qpoTKxC.jpg",
+        nome:"Senhor dos Anéis"
+    },
+
+    {
+        link:"https://image.tmdb.org/t/p/w220_and_h330_face/oktTNFM8PzdseiK1X0E0XhB6LvP.jpg",
+        nome:"Vikings"
+    },
+
+    {
+        link:"https://image.tmdb.org/t/p/w220_and_h330_face/iSNdwFauC1QODm1ntk07wqJV1pf.jpg",
+        nome:"Star Wars"
+    },
+
+    {
+        link:"https://image.tmdb.org/t/p/w220_and_h330_face/oaLXsduGoDOOftWziIOvyZzrR4T.jpg",
+        nome: "Arrow"
+    }, 
+
+    {
+        link:"https://image.tmdb.org/t/p/w220_and_h330_face/xLxgVxFWvb9hhUyCDDXxRPPnFck.jpg",
+        nome:"joker"
+    },
+
+    {
+        link:"https://image.tmdb.org/t/p/w220_and_h330_face/geyu6rplpbp7OUeOfB2uRVf1LpG.jpg",
+        nome:"Liga da Justiça"
+    },
+
+    {
+        link:"https://image.tmdb.org/t/p/w220_and_h330_face/f496cm9enuEsZkSPzCwnTESEK5s.jpg",
+        nome:"Friends"
+    },
+
+    {
+        link:"https://image.tmdb.org/t/p/w220_and_h330_face/811j0Jf2D0mK1U6RxXJoZgOB29n.jpg",
+        nome:"Harry Potter"
+    },
+
+    {
+        link:"https://image.tmdb.org/t/p/w220_and_h330_face/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg",
+        nome: "Flash"
+    },
+
+    {
+        link:"https://image.tmdb.org/t/p/w220_and_h330_face/38teDX74nsxkv2ysWvNT5EPXQ9E.jpg",
+        nome:"Power"
+    },
+
+    {
+        link:"https://image.tmdb.org/t/p/w220_and_h330_face/64gq5GavH3wAIxbFeyE5qpoTKxC.jpg",
+        nome:"Senhor dos Anéis"
+    },
+
+    {
+        link:"https://image.tmdb.org/t/p/w220_and_h330_face/oktTNFM8PzdseiK1X0E0XhB6LvP.jpg",
+        nome:"Vikings"
+    },
+
+    {
+        link:"https://image.tmdb.org/t/p/w220_and_h330_face/iSNdwFauC1QODm1ntk07wqJV1pf.jpg",
+        nome:"Star Wars"
+    }]   
+
+    urlsCartaz.forEach ((valorAtual) => {
+        filmes.innerHTML += `<div class="item"> <img class="box-filme" src="${valorAtual.link}" alt="${valorAtual.nome}"></div>`
+    });
+
+
+
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
